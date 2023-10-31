31b99a15bfddad77cf2a32ed71464a9a0f5fb7ea
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2023-10-31 08:35:20 -0600

    added assemblerinjector

--- Dogos/Modules/Dogs/Presentation/DogsViewController.swift
+++ Dogos/Modules/Dogs/Presentation/DogsViewController.swift
@@ -1,9 +1,30 @@
 import UIKit
 
 class DogsViewController: UIViewController {
+  private let assemblerInjector = DogsAssemblerInjector()
+  private var presenter: DogsPresenterProtocol!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    initializePresenter()
+  }
+
+  private func initializePresenter() {
+    presenter = assemblerInjector.resolve(dogsUIProtocol: self)
+  }
+
+  private func fetchDogs() async {
+    await presenter.fechtDogs()
   }
 }
 
+extension DogsViewController: DogsUIProtocol {
+
+  func showError(_ error: String) { }
+
+  func showLoader() { }
+
+  func hideLoader() { }
+
+  func diplayUserData(_ dogs: [Dog]) { }
+}
