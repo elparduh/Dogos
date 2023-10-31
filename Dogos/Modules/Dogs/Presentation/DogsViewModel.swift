99d2d496e3a2d7b00cc5680a5c6911a8667415c8
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2023-10-31 10:10:11 -0600

    06-Add-View-Model - UI (#6)

---
+++ Dogos/Modules/Dogs/Presentation/DogsViewModel.swift
@@ -0,0 +1,48 @@
+import Foundation
+import Combine
+
+protocol DogsViewModelProtocol {
+  var state: PassthroughSubject<StateController, Never> { get }
+  var dogsUseCaseProviderProtocol: DogsUseCaseProviderProtocol { get }
+  var dogsItemsCount: Int { get }
+  func viewDidLoad()
+}
+
+class DogsViewModel: DogsViewModelProtocol {
+  private var dogItems: [Dog] = []
+  var dogsUseCaseProviderProtocol: DogsUseCaseProviderProtocol
+  var state: PassthroughSubject<StateController, Never>
+
+  var dogsItemsCount: Int {
+    dogItems.count
+  }
+
+  init(dogsUseCaseProviderProtocol: DogsUseCaseProviderProtocol, state: PassthroughSubject<StateController, Never>) {
+    self.dogsUseCaseProviderProtocol = dogsUseCaseProviderProtocol
+    self.state = state
+  }
+
+  func viewDidLoad() {
+    state.send(.loading)
+    Task {
+      let result = await dogsUseCaseProviderProtocol.getDogsUseCaseProvider().execute()
+        updateUI(result: result)
+    }
+  }
+  
+  private func updateUI(result: Result<[Dog], Error>) {
+      switch result {
+      case .success(let dogs):
+          dogItems = dogs
+          state.send(.success)
+      case .failure(let error):
+          state.send(.fail(error: error.localizedDescription))
+      }
+  }
+}
+
+enum StateController {
+    case success
+    case loading
+    case fail(error: String)
+}
